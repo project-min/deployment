844ff19b50be6b7756110e1af4aaf55ba24d961a
Ye Min <project.min@outlook.com>
2019-04-13 23:05:14 +0800

    Set the variable.

--- update_mysql_password.sh
+++ update_mysql_password.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.policy=0;"
-mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.length=4;"
-mysql -u "$userid" -p"$temporary_password" -e "set password=\"$password\";"
+userid='root'
+temporary_password_string=`grep "temporary password" /var/log/mysqld.log`
+temporary_password=${temporary_password_string##* }
+if [ x"$1" = x ]; then
+    password="test-123"
+else
+    password=$1
+fi
+
+echo $temporary_password
+echo $password
+
+mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.policy=0;" --connect-expired-password
+mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.length=4;" --connect-expired-password
+mysql -u "$userid" -p"$temporary_password" -e "set password=\"$password\";" --connect-expired-password
