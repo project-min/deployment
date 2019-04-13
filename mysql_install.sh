1e6d32db7f6d36e2e1a5012798a4e8833fe2c91f
Ye Min <project.min@outlook.com>
2019-04-13 22:26:27 +0800

    Add double quote for the variable.

--- mysql_install.sh
+++ mysql_install.sh
@@ -18,8 +18,8 @@ fi
 echo $temporary_password
 echo $password
 
-mysql -u ${userid} -p${temporary_password} -e "set global validate_password.policy=0;"
-mysql -u ${userid} -p${temporary_password} -e "set global validate_password.length=4;"
-mysql -u ${userid} -p${temporary_password} -e "set password=\"${password}\";"
+mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.policy=0;"
+mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.length=4;"
+mysql -u "$userid" -p"$temporary_password" -e "set password=\"$password\";"
 
 yum install mysqlclient
