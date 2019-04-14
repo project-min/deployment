9057f19dc4ac6b42b036f0fb3915ceeab5103613
Ye Min <project.min@outlook.com>
2019-04-14 14:56:34 +0800

    Add the command to update the password.

--- mysql_install.sh
+++ mysql_install.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+# The first parameter is the new password
+# ./mysql_install.sh test-123
+
+# The default password will be test-123 if no parameter
+# ./mysql_install.sh
+
 wget https://repo.mysql.com//mysql80-community-release-el7-1.noarch.rpm
 rpm -Uvh mysql80-community-release-el7-1.noarch.rpm
 yum install mysql-community-server -y
+
 service mysqld start
 service mysqld status
 
 userid='root'
 temporary_password_string=`grep "temporary password" /var/log/mysqld.log`
 temporary_password=${temporary_password_string##* }
-#if [ x"$1" = x ]; then
-#    password="test-123"
-#else
-#    password=$1
-#fi
+if [ x"$1" = x ]; then
+    password="test-123"
+else
+    password=$1
+fi
 
+echo $password
+
+mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.policy=0;" --connect-expired-password
+mysql -u "$userid" -p"$temporary_password" -e "set global validate_password.length=4;" --connect-expired-password
+mysql -u "$userid" -p"$temporary_password" -e "set password=\"$password\";" --connect-expired-password
+
+echo "Temporary password:"
 echo $temporary_password
-#echo $password
 
-
-yum install mysqlclient
+echo "New password:"
+echo $password
